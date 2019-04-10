b5b75f4ed62a62e4ca9e6ae19f4149229416306e
Kamlesh Katpara <mekamleshk@gmail.com>
2019-04-11 02:52:34 +0530

    Student Assessment UI Done

--- pages/studentassessments.vue
+++ pages/studentassessments.vue
@@ -21,6 +21,7 @@
                     <v-autocomplete
                       v-model="student"
                       :items="studentitems"
+                      :error-messages="studentErrors"
                       :search-input.sync="searchStudent"
                       hide-no-data
                       hide-selected
@@ -32,6 +33,7 @@
                     <v-autocomplete
                       v-model="assessment"
                       :items="assessmentitems"
+                      :error-messages="assessmentErrors"
                       :search-input.sync="searchAssessment"
                       hide-no-data
                       hide-selected
@@ -43,6 +45,7 @@
                     <v-autocomplete
                       v-model="volunteer"
                       :items="volunteeritems"
+                      :error-messages="volunteerErrors"
                       :search-input.sync="searchVolunteer"
                       hide-no-data
                       hide-selected
@@ -53,40 +56,44 @@
 
                     <v-layout wrap>
                       <v-flex xs12 sm6 md6>
-                        <v-text-field v-model="score" placeholder="Please enter score"></v-text-field>
+                        <v-text-field
+                          v-model="score"
+                          :error-messages="scoreErrors"
+                          placeholder="Please enter score"
+                        ></v-text-field>
                       </v-flex>
 
                       <v-flex xs12 sm6 md6>
-                          <v-menu
-                            ref="menu"
-                            v-model="menu"
-                            :close-on-content-click="false"
-                            :nudge-right="40"
-                            lazy
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px"
-                          >
-                            <template v-slot:activator="{ on }">
-                              <v-text-field
-                                v-model="date"
-                                placeholder="Assessment date"
-                                prepend-icon="event"
-                                readonly
-                                v-on="on"
-                              ></v-text-field>
-                            </template>
-                            <v-date-picker
-                              ref="picker"
-                              v-model="date"
-                              :max="new Date().toISOString().substr(0, 10)"
-                              min="1950-01-01"
-                              @change="save"
-                            ></v-date-picker>
-                          </v-menu>
+                        <v-menu
+                          ref="menu"
+                          v-model="menu"
+                          :close-on-content-click="false"
+                          :nudge-right="40"
+                          lazy
+                          transition="scale-transition"
+                          offset-y
+                          full-width
+                          min-width="290px"
+                        >
+                          <template v-slot:activator="{ on }">
+                            <v-text-field
+                              v-model="assessmentdate"
+                              :error-messages="assessmentDateErrors"
+                              placeholder="Assessment date"
+                              prepend-icon="event"
+                              readonly
+                              v-on="on"
+                            ></v-text-field>
+                          </template>
+                          <v-date-picker
+                            ref="picker"
+                            v-model="assessmentdate"
+                            :max="new Date().toISOString().substr(0, 10)"
+                            min="1950-01-01"
+                            @change="save"
+                          ></v-date-picker>
+                        </v-menu>
                       </v-flex>
-                      
                     </v-layout>
                   </v-flex>
                 </v-layout>
@@ -109,20 +116,50 @@
         </v-card>
       </v-dialog>
     </v-toolbar>
+    <v-snackbar v-model="snackbar" :color="color" :timeout="timeout" top>
+      {{ this.status
+      }}
+      <v-icon dark size="15" @click="snackbar = false">fas fa-times fa-xs</v-icon>
+    </v-snackbar>
   </div>
 </template>
 
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, helpers } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
+  validations: {
+    student: {
+      required
+    },
+    assessment: {
+      required
+    },
+    volunteer: {
+      required
+    },
+    score: {
+      required
+    },
+    assessmentdate: {
+      required
+    }
+  },
   data: () => ({
+    snackbar: false,
+    status: '',
+    color: '',
+    timeout: 6000,
     student: null,
     searchStudent: null,
     assessment: null,
     searchAssessment: null,
     volunteer: null,
     score: '',
-    date: null,
+    assessmentdate: null,
     menu: false,
     searchVolunteer: null,
     addDialog: false,
@@ -130,8 +167,38 @@ export default {
     loader: null,
     loading: false
   }),
-
   computed: {
+    studentErrors() {
+      const errors = []
+      if (!this.$v.student.$dirty) return errors
+      !this.$v.student.required && errors.push('Please select student')
+      return errors
+    },
+    assessmentErrors() {
+      const errors = []
+      if (!this.$v.assessment.$dirty) return errors
+      !this.$v.assessment.required && errors.push('Please select assessment')
+      return errors
+    },
+    volunteerErrors() {
+      const errors = []
+      if (!this.$v.volunteer.$dirty) return errors
+      !this.$v.volunteer.required && errors.push('Please select volunteer')
+      return errors
+    },
+    scoreErrors() {
+      const errors = []
+      if (!this.$v.score.$dirty) return errors
+      !this.$v.score.required && errors.push('Please enter score')
+      return errors
+    },
+    assessmentDateErrors() {
+      const errors = []
+      if (!this.$v.assessmentdate.$dirty) return errors
+      !this.$v.assessmentdate.required &&
+        errors.push('Please select assessment date')
+      return errors
+    },
     studentitems() {
       return this.studentnames.map(entry => {
         const Studentname = entry.name
@@ -162,14 +229,43 @@ export default {
   },
   methods: {
     send() {
-      console.log('student id => ' + this.student.id)
-      console.log('assessment id => ' + this.assessment.id)
-      console.log('assessment total_score => ' + this.assessment.total_score)
-      console.log('volunteer id => ' + this.volunteer.id)
-      console.log('volunteer id => ' + this.date)
+      if (
+        !this.$v.student.$invalid &&
+        !this.$v.assessment.$invalid &&
+        !this.$v.volunteer.$invalid &&
+        !this.$v.score.$invalid &&
+        !this.$v.assessmentdate.$invalid
+      ) {
+        if (this.score > this.assessment.total_score) {
+          this.snackbar = true
+          this.color = 'red darken-4'
+          window.navigator.vibrate(200)
+          this.status = 'Entered score is greater than Assessment Score !'
+        } else {
+          this.$store.dispatch('addStudentAssessment', {
+            student_id: this.student.id,
+            assessment_id: this.assessment.id,
+            volunteer_id: this.volunteer.id,
+            score: this.score,
+            assessment_date: this.assessmentdate
+          })
+          this.addDialog = false
+          setTimeout(() => {
+            this.$store.dispatch('getStudentAssessments')
+          }, 700)
+        }
+      } else if (
+        this.$v.student.$invalid &&
+        this.$v.assessment.$invalid &&
+        this.$v.volunteer.$invalid &&
+        this.$v.score.$invalid &&
+        this.$v.assessmentdate.$invalid
+      ) {
+        this.$v.$touch()
+      }
     },
-    save (date) {
-      this.$refs.menu.save(date)
+    save(assessmentdate) {
+      this.$refs.menu.save(assessmentdate)
     }
   },
   watch: {
@@ -185,7 +281,7 @@ export default {
       if (this.volunteeritems.length > 0) return
       this.$store.dispatch('getVolunteerNames')
     },
-    menu (val) {
+    menu(val) {
       val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
     }
   }
